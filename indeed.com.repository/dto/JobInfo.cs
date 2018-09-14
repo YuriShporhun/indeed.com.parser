f04b570789f10177e5f493919fb017e64c7ead33
Юрий <YuriPCLive@live.ru>
2018-09-14 23:40:21 +0300

    Job info docs

--- indeed.com.repository/dto/JobInfo.cs
+++ indeed.com.repository/dto/JobInfo.cs
@@ -11,12 +11,48 @@ namespace indeed.com.models
     /// </summary>
     public class JobInfo
     {
+        /// <summary>
+        /// ID of a job info 
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Company's name which posted a job
+        /// </summary>
         public string CompanyName { get; set; }
+
+        /// <summary>
+        /// Description of a job
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Tittle of a job
+        /// </summary>
+        /// <remarks>
+        /// It's usually a job name and some basic job info
+        /// </remarks>
         public string Title { get; set; }
+
+        /// <summary>
+        /// City where a job is located
+        /// </summary>
         public string City { get; set; }
+
+        /// <summary>
+        /// Posting time of a job
+        /// </summary>
+        /// <remarks>
+        /// It's usually in format like 'N-time ago'
+        /// </remarks>
         public string PostingTime { get; set; }
+
+        /// <summary>
+        /// Salary if a job
+        /// </summary>
+        /// <remarks>
+        /// It basically is the ExpectedSalary field from ParserRequest
+        /// </remarks>
         public int ApproxSalary { get; set; }
     }
 }
